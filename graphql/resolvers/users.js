385e7a6076fb4170ffd5aba46ad1c5807f146586
OrchaniousS <bmagramovs3@gmail.com>
2021-04-16 00:43:01 +0300

    graphql register and login

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,89 @@
+require('dotenv').config({ path: '.env' });
+const { UserInputError } = require('apollo-server-errors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const {validateRegisterInput, validateLoginInput} = require('../../util/validators')
+
+const User = require('../../models/User');
+
+const generateToken = (user) => {
+    return jwt.sign({
+        id:res.id,
+        email:res.email,
+        username:res.username
+    }, process.env.JWT_TOKEN, {expiresIn:'1h'});
+}
+
+module.exports = {
+    Mutation: {
+        async login(_,
+            {username,password}) {
+                const {errors,valid} = validateLoginInput(username,password);
+                const user = await User.findOne({username})
+
+                if(!valid){
+                    throw new UserInputError('Errors',{errors})
+                }
+
+                if(!user){
+                    errors.general = 'User not found'
+                    throw new UserInputError('User not found',{errors})
+                }
+
+                const match = await bcrypt.compare(password,user.password)
+
+                if(!match){
+                    errors.general = 'Wrong credentials';
+                    throw new UserInputError('Wrong credentials',{errors})
+                }
+
+                const token = generateToken(user);
+
+                return {
+                    ...user._doc,
+                    id:user._id,
+                    token
+                }
+            },
+        async register(_,
+            {registerInput:{username,email,password,confirmPassword}},
+            ){
+            // Validation
+            const {valid, errors} = validateRegisterInput(username,email,password,confirmPassword);
+            if(!valid){
+                throw new UserInputError('Errors',{errors})
+            }
+            
+            // Hash password
+            password = await bcrypt.hash(password,12);
+
+           // Check if user exists in db
+            const user = User.findOne({username})
+
+            if(user){
+                throw new UserInputError('username is taken',{
+                    errors:{
+                        username:'This username is taken'
+                    }
+                })
+            }
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString(),
+            });
+
+            const res = await newUser.save();
+
+            const token = generateToken(user)
+
+            return {
+                ...res._doc,
+                id:res._id,
+                token
+            }
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,43 +1,15 @@
 require('dotenv').config({ path: '.env' });
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
+// const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post')
-const User = require('./models/User')
-
-const typeDefs = gql`
-    type Post{
-        id:ID!
-        body:String!
-        createdAt:String!
-        username:String!
-    }
-    type Query{
-        sayHi: String!,
-        getPosts:[Post]
-    }
-`
-
-const resolvers = {
-    Query:{
-        sayHi: () => 'Hellow',
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            } catch(err){
-                throw new Error(err);
-            }
-        }
-    }
-}
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers/index')
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
-
 
 mongoose
 .connect(process.env.MONGO_URI,{
